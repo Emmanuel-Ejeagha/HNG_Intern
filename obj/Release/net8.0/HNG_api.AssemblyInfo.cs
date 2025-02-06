1038422e5cc4fb9324acd5ea3df1cf9213ffee98
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-02-06 17:12:40 +0100

    Updated

--- obj/Release/net8.0/HNG_api.AssemblyInfo.cs
+++ obj/Release/net8.0/HNG_api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HNG_api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3afabe5e8a64f6ec85a1ac8e28b159fa969f214")]
 [assembly: System.Reflection.AssemblyProductAttribute("HNG_api")]
